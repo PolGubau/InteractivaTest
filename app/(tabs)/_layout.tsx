76649603eac1f5345dcb445cacf83aa720314ad7
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-26 19:27:10 +0100

    feat(UI): ✨ implement responsive design for user screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
@@ -6,6 +7,7 @@ import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
+
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
@@ -17,9 +19,11 @@ export default function TabLayout() {
 				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
 				headerShown: false,
 				tabBarButton: HapticTab,
+				headerTintColor: "#fff",
 				tabBarBackground: TabBarBackground,
 				tabBarStyle: Platform.select({
 					ios: {
+						// Use a transparent background on iOS to show the blur effect
 						position: "absolute",
 					},
 					default: {},
@@ -30,26 +34,28 @@ export default function TabLayout() {
 				name="index"
 				options={{
 					title: "Home",
-					tabBarIcon: ({ color }) => (
-						<IconSymbol size={28} name="house.fill" color={color} />
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							name={focused ? "home-sharp" : "home-outline"}
+							color={color}
+							size={24}
+						/>
 					),
+					// tabBarIcon: ({ color }) => (
+					// 	<IconSymbol size={28} name="house.fill" color={color} />
+					// ),
 				}}
 			/>
 			<Tabs.Screen
 				name="explore"
 				options={{
 					title: "Explore",
-					tabBarIcon: ({ color }) => (
-						<IconSymbol size={28} name="paperplane.fill" color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="user"
-				options={{
-					title: "User",
-					tabBarIcon: ({ color }) => (
-						<IconSymbol size={28} name="person.fill" color={color} />
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							name={focused ? "document-text" : "document-outline"}
+							color={color}
+							size={24}
+						/>
 					),
 				}}
 			/>
